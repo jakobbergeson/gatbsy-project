ac19d23abcd2724e30792ebf95b762deca5b6d49
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2021-04-28 15:48:35 -0700

    Added logic to render images from contentfuls rich text

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import { renderRichText } from "gatsby-source-contentful/rich-text"
+import { BLOCKS } from "@contentful/rich-text-types"
 
 //This was used to query data from local markdown files to be rendered below
 ///////////////////////////////
@@ -25,17 +26,34 @@ export const query = graphql`
       publishedDate(formatString: "MMMM Do, YYYY")
       body {
         raw
+        references {
+          ... on ContentfulAsset {
+            __typename
+            contentful_id
+            fixed {
+              src
+            }
+          }
+        }
       }
     }
   }
 `
+
+const options = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: node => {
+      return <img src={node.data.target.fixed.src} alt="bandage"></img>
+    },
+  },
+}
 
 const Blog = props => {
   return (
     <Layout>
       <h1>{props.data.contentfulBlogPost.title}</h1>
       <p> {props.data.contentfulBlogPost.publishedDate}</p>
-      {renderRichText(props.data.contentfulBlogPost.body)}
+      {renderRichText(props.data.contentfulBlogPost.body, options)}
     </Layout>
   )
 }
